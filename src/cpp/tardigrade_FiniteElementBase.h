082dddd7d4e3661d01347582a0606db1f2ac3d28
Nathan Miller <nami2227@colorado.edu>
2026-03-10 11:02:10 -0600

    MAINT: Added virtual function for getting the surface integration point information

--- src/cpp/tardigrade_FiniteElementBase.h
+++ src/cpp/tardigrade_FiniteElementBase.h
@@ -45,8 +45,9 @@ namespace tardigradeBalanceEquations {
             virtual void GetVolumeIntegrationPointData(const unsigned int i, local_point_out xi_begin,
                                                        local_point_out xi_end, weight_type &weight);
 
-            virtual void GetSurfaceIntegrationPointData(const unsigned int s, const unsigned int i, local_point_out xi_begin,
-                                                        local_point_out xi_End, weight_type &weight);
+            virtual void GetSurfaceIntegrationPointData(const unsigned int s, const unsigned int i,
+                                                        local_point_out xi_begin, local_point_out xi_end,
+                                                        weight_type &weight);
 
             template <class quantity_in, class quantity_out>
             void InterpolateQuantity(const local_point_in &xi_begin, const local_point_in &xi_end,
